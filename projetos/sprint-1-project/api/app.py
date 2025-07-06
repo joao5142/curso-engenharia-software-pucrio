8c2995548350a175d8694ecc5e68bd3cc6d421b3
João Paulo <45145741+joao5142@users.noreply.github.com>
2025-07-05 23:29:25 -0300

    feat: init openapi docs

--- projetos/sprint-1-project/api/app.py
+++ projetos/sprint-1-project/api/app.py
@@ -1,5 +1,5 @@
-from flask_openapi3 import OpenAPI, Info
-
+from flask_openapi3 import OpenAPI, Info, Tag
+from flask import redirect
 from flask_cors import CORS
 
 info = Info(title="Sprint 1 API", version="1.0.0")
@@ -7,3 +7,8 @@ app = OpenAPI(__name__, info=info)
 
 CORS(app)
 
+openapi_documentation_tag = Tag(name="Projeto Sprint 1", description="Documentação da API do projeto Sprint 1")
+
+@app.get('/', tags=[openapi_documentation_tag])
+def home():
+    return redirect('/openapi')
