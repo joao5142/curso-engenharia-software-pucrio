d0e80b81622de550a2e1e1a93bf1c5943ca592be
joao5142 <joaopauloneto3687@gmail.com>
2025-07-07 10:42:55 -0300

    first commit

--- projetos/sprint-1-project/api/schemas/__init__.py
+++
@@ -1,2 +0,0 @@
-from schemas.client import ClientSchema, GetClientSchema, GetAllClientsSchema, ClientParamsSchema, GetClientsQuantitySchema, show_clients, show_client
-from schemas.error import ErrorSchema
